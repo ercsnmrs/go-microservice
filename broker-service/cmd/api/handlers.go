915b8d5e2093d1f9927ba8178511db900eb0d8f6
Ericson <ericsonmuros@gmail.com>
2022-11-15 13:20:11 +0800

    implement helpers func and updated frontend.
    - implement helper servc
    - simplified broker func with helper
    - create js test function to retrieve data
    [BROKER]

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-// Go 1.18 version can use `any` data type otherwise use `interface{}`
-type jsonResponse struct {
-	Error   bool        `json:"error"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -18,8 +10,11 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
+
+	// Simplified by the json helper func writeJSON
+	// out, _ := json.MarshalIndent(payload, "", "\t")
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusAccepted)
+	// w.Write(out)
 }
