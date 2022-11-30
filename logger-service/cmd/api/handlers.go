727cc81dee11a4871d9501b2e1587db8a3306f99
Ericson <ericsonmuros@gmail.com>
2022-11-30 23:58:17 +0800

    set router, handler, and web server for log service
    [LOGGER]

---
+++ logger-service/cmd/api/handlers.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log-service/data"
+	"net/http"
+)
+
+type JsonPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	// read JSON into a var
+	var requestPayload JsonPayload
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// insert data
+	event := data.LogEntry{
+		Name: requestPayload.Name,
+		Data: requestPayload.Data,
+	}
+
+	err = app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, http.StatusAccepted, resp)
+}
