c66ec45766bc6e96586943ed3cf8a6d65e7d315b
Ericson <ericsonmuros@gmail.com>
2023-01-03 00:08:57 +0800

    listening rpc calls for the logger microservice
    - register RPC Server
    - declare start of RPC Server
    
    [RPC]

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -49,6 +51,11 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	// start RPC Server 
+	go app.rpcListen()
+
 	// start web server
 	// go app.serve()
 	log.Println("Starting service on port!", webPort)
@@ -74,6 +81,24 @@ func main() {
 // 		log.Panic(err)
 // 	}
 // }
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC Server on Port ", rpcPort)
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	// can read the mongo driver documentation
